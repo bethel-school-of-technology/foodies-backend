8e64e30b3563a22cd639189f6155731585f21d27
Lauren <laurentepaske@gmail.com>
2019-08-15 13:10:44 -0600

    user routes almost done. Logout route unfinished

--- routes/users.js
+++ routes/users.js
@@ -3,23 +3,11 @@ require('dotenv').config();
 var router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const checkAuth = require("../middleware/check-auth");
 
 var Users = require('../models/users');
 
 //Sign up (CREATE user)
-
-router.post('/test', (req, res) => {
-    const user = new Users({
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email
-    });
-    user.save((err, response)=>{
-        if(err) res.status(400).send(err)
-        res.status(200).send(response)
-    });
-    console.log(user);
-})
 
 router.post('/signup', (req, res) => {
     bcrypt.hash(req.body.password, 10)
@@ -37,86 +25,7 @@ router.post('/signup', (req, res) => {
         });
     });
 
-// router.post('/signup', (req, res, next) => {
-//     bcrypt.hash(req.body.password, 10)
-//     .then(hash => {
-//         const user = new Users({
-//             username: req.body.username,
-//             password: hash,
-//             email: req.body.email
-//         });
-//         user.save()
-//         .then(result => {
-//             res.status(201).json({
-//                 message: 'user created!',
-//                 result: result
-//             });
-//         })
-//         .catch(err => {
-//             res.status(500).json({
-//                 error: err
-//             });
-//         });
-//     });
-//   });
-
 //Log in 
-// router.post('/login', function(req, res, next) {
-//     let fetchedUser;
-//     Users.findOne({ email: req.body.email }).then(user => {
-//         if (!user) {
-//             return res.status(401).json({
-//                 message:"Auth Failed"
-//             });
-//         }
-//         let fetchedUser = user;
-//         return bcrypt.compare(req.body.password, user.password);
-//     })
-//     .then(result => {
-//         if (!result) {
-//             return res.status(401).json({
-//                 message: "Auth Failed"
-//              });
-//         }
-//         const token = jwt.sign(
-//             {username: fetchedUser.username, userId: fetchedUser._id}, 
-//             process.env.SECRET,
-//         { expiresIn: '1h' }
-//         );
-//         res.status(200).json({
-//             token: token
-//         });
-//     })
-//     .catch(err => {
-//         return res.status(401).json({
-//             message: "Auth Failed"
-//         });
-//     });
-//   });  
-
-//   router.post('/login', function(req, res, next) {
-//     Users.findOne({ email: req.body.email, username: req.body.username}).then(user => {
-//         if (!user) {
-//             return res.status(401).json({
-//                 message:"Auth Failed"
-//             });
-//         };
-//         if (bcrypt.compare(req.body.password, user.password)) {
-//             const token = jwt.sign(
-//                 {username: user.username, userId: user._id}, 
-//                 process.env.SECRET,
-//             { expiresIn: '1h' }
-//             );
-//             res.status(200).json({
-//                 token: token
-//             });
-//         } else {
-//             return res.status(401).json({
-//                 message: "auth failed"
-//             });
-//         }
-//     });
-//   });
 
   router.post('/login', function(req, res, next) {
     Users.findOne({ email: req.body.email, username: req.body.username}).then(user => {
@@ -125,14 +34,7 @@ router.post('/signup', (req, res) => {
                 message:"Auth Failed"
             });
         };
-
-        bcrypt.compare(req.body.password, user.password, (response, err) => {
-            if(err){
-                return res.status(401).json({
-                    message:"Auth Failed"
-               });
-            }
-
+        if (bcrypt.compare(req.body.password, user.password)) {
             const token = jwt.sign(
                 {username: user.username, userId: user._id}, 
                 process.env.SECRET,
@@ -140,15 +42,29 @@ router.post('/signup', (req, res) => {
             );
             res.status(200).json({
                 token: token
-            })
-        });
-    })
-});
+            });
+        } else {
+            return res.status(401).json({
+                message: "auth failed"
+            });
+        }
+    });
+  });
 
 //GET profile page
-// router.get('/profile/:id', function(req, res, next) 
+// router.get('/profile/:id', checkAuth function(req, res, next) 
 //     res.send('respond with a resource');
 //   });
+
+router.get("/profile/:_id", (req, res, next) => {
+    Users.findById(req.params._id).then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({message: "User not found!"});
+      }
+    });
+  });
 
 //log out
 
